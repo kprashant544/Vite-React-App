ba10d3ca7a09b5bc6e978e1276900492c87138e7
kprashant544 <kasulaprashant544@gmail.com>
2024-02-05 15:04:23 +0545

    card modified

--- src/PracticeThings/WeddingCard.jsx
+++ src/PracticeThings/WeddingCard.jsx
@@ -1,23 +1,47 @@
 import "../PracticeThings/weddingcard.css";
 
 function WeddingCard(props) {
-  const { children, title, descrip } = props;
+  const { children, title, descriptions, groom, bride, and } = props;
   const ganesh = "./ganesh-logo.png";
   const shuva = "./shuva-biwaha.png";
+  const bridegroom = "./bride-groom.png";
+
   return (
     <>
       <div className="wed-card">
-        <div className="wed">{title}</div>
+        <div className="wed">
+          <h2>{title}</h2>
+        </div>
         <div className="images">
-          <div>
+          <div className="img-1">
             <img src={ganesh} style={{ height: "250px", width: "250px" }} />
           </div>
-          <div>
+          <div className="img-2">
             <img src={shuva} style={{ height: "250px", width: "250px" }} />
           </div>
         </div>
         {children} <br></br>
-        {descrip}
+        <h3>{}</h3>
+        <h2>{groom}</h2>
+        <h2>{and}</h2>
+        <h2>{bride}</h2>
+        <span className="dis1">
+          <h3>{descriptions.desc1}</h3>
+        </span>
+        <h4>{descriptions.desc2}</h4>
+        <span className="dis2">
+          <h3>{descriptions.desc3}</h3>
+        </span>
+        <h4>{descriptions.desc4}</h4>
+        <h4>{descriptions.desc5}</h4>
+        <span className="dis3">
+          <h3>{descriptions.desc6}</h3>
+        </span>
+        <h4>{descriptions.desc7}</h4>
+        <span className="dis4">
+          <h3>{descriptions.desc8}</h3>
+        </span>
+        <h4>{descriptions.desc9}</h4>
       </div>
     </>
   );
--- src/WebLayout/Layout.jsx
+++ src/WebLayout/Layout.jsx
@@ -30,12 +30,12 @@ function Layout() {
     },
     {
       title: "Card no.4",
-      img: "https://picsum.photos/id/870/200?grayscale&blur=1",
+      img: "https://picsum.photos/200?grayscale&blur=1",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
       title: "Card no.5",
-      img: "https://picsum.photos/id/870/200?grayscale&blur=1",
+      img: "https://picsum.photos/200?grayscale",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
@@ -60,6 +60,18 @@ function Layout() {
     },
   ];
 
+  const descriptions = {
+    desc1: "Cocktail & Dinner",
+    desc2: "Sunday, 25th Feb 2024",
+    desc3: "Reception & Wedding",
+    desc4: "Monday, 26th Feb 2024",
+    desc5: "7 o'clock in the evening",
+    desc6: "Venue:",
+    desc7: "Marriott Hotel , Kathmandu",
+    desc8: "Regards",
+    desc9: "Beck Smith & Smith Family",
+  };
+
   return (
     <div>
       <Nav meet={meet} />
@@ -75,11 +87,19 @@ function Layout() {
         ))}
       </div>
       <Button />
-      <WeddingCard title={"Wedding Card"} descrip={"Wedding Of"}>
-        You are cordially invited to the
+
+      <WeddingCard
+        title={"Wedding Card"}
+        descrip={"Wedding Of"}
+        groom={"Helen"}
+        bride={"Mary"}
+        and={"&"}
+        descriptions={descriptions}
+      >
+        <h3>You are cordially invited to the</h3>
       </WeddingCard>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
